487bb4e40771b6e20b0ade4067fdf9813ac08445
nishaniC <145260442+nishaniC@users.noreply.github.com>
2025-12-10 09:02:07 -0500

    Add @Override annotation to reduce method

--- PrecipitationReducer.java
+++ PrecipitationReducer.java
@@ -4,7 +4,9 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class PrecipitationReducer extends Reducer<Text, FloatWritable, Text, FloatWritable> {
-    public void reduce(Text key, Iterable<FloatWritable> values, Context context) throws IOException, InterruptedException {
+    @Override
+    public void reduce(Text key, Iterable<FloatWritable> values, Context context)
+            throws IOException, InterruptedException {
         float total = 0;
         for (FloatWritable val : values) {
             total += val.get();
